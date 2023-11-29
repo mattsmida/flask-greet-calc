3f9b55fb925810738bb743c339fbe647f82c0e0e
Matt Smida <mcs@m4t7.com>
2023-11-29 15:15:51 -0500

    initial request handlers in app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.get('/welcome')
+def welcome():
+    return "welcome"
+
+@app.get('/welcome/home')
+def welcome_home():
+    return "welcome home"
+
+@app.get('/welcome/back')
+def welcome_back():
+    return "welcome back"
+
+
